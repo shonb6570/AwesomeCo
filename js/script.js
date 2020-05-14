65ac119a17b5924f7f0b2e34c8d549838c1b64b1
Shaun Bolak <47017803+shonb6570@users.noreply.github.com>
2020-05-14 15:56:40 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Fetch functions
+
+function createUser(element) {
+    return document.createElement(element);
+}
+
+
+function appendElement(parent, element) {
+    return document.appendChild(element);
+}
+
+
+// fetch request
+fetch('https://randomuser.me/api/?results=12')
+    .then(function (data) {
+
+    })
+    .catch(function (error) {
+
+    });
+
+// get employee name from ajax request
+
+// store employee-card div in a variable
+
+//create <p> element and append employee name
+
+
+//repeat for image, first and last name, email and city
+
+
+//create a modal window
+
+//add the following extra info in modal only - cell, detailed address and birth date
