2d4b57c1430364cd8e3d497702f8c7b59e3bfbc7
Shaun Bolak <47017803+shonb6570@users.noreply.github.com>
2020-05-15 14:48:44 -0400

    fetch data received/displayed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+// Fetch variables 
+
+const employees = document.getElementById("employees");
+const url = 'https://randomuser.me/api/?results=12';
+
 // Fetch functions
 
 function createUser(element) {
@@ -5,30 +10,35 @@ function createUser(element) {
 }
 
 
-function appendElement(parent, element) {
-    return document.appendChild(element);
-}
-
-
-// fetch request
-fetch('https://randomuser.me/api/?results=12')
+function fetchInfo(url){
+    fetch(url)
+    .then((response) => (response.json()))
     .then(function (data) {
-
+        console.log(data);
+        data.results.forEach(employee => {
+            let li = createUser('li');
+            let img = createUser('img');
+            let name = createUser('p');
+            let email = createUser('p');
+            let location = createUser('p');
+            img.src = employee.picture.medium;
+            name.innerHTML = `${employee.name.first} ${employee.name.last}`;
+            email.innerHTML = `${employee.email}`;
+            location.innerHTML = `${employee.location.city}`;
+            li.appendChild(img);
+            li.appendChild(name);
+            li.appendChild(email);
+            li.appendChild(location);
+            employees.appendChild(li);
+            li.classList.add("employee-card");
+        })
     })
     .catch(function (error) {
-
+        console.log(error);
     });
+}
 
-// get employee name from ajax request
-
-// store employee-card div in a variable
-
-//create <p> element and append employee name
-
-
-//repeat for image, first and last name, email and city
-
-
-//create a modal window
-
-//add the following extra info in modal only - cell, detailed address and birth date
+// fetch request
+document.addEventListener('DOMContentLoaded', () => {
+    fetchInfo(url);
+});
