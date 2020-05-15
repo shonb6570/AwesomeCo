01aa38b1983114794574b8055cb869942c9aa07e
Shaun Bolak <47017803+shonb6570@users.noreply.github.com>
2020-05-15 16:49:08 -0400

    updating js to format user info in employee card

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // Fetch variables 
 
+        //"employees" is a <ul> element
 const employees = document.getElementById("employees");
 const url = 'https://randomuser.me/api/?results=12';
 
@@ -16,20 +17,32 @@ function fetchInfo(url){
     .then(function (data) {
         console.log(data);
         data.results.forEach(employee => {
+            //create html elements for API employee data  
             let li = createUser('li');
             let img = createUser('img');
             let name = createUser('p');
             let email = createUser('p');
             let location = createUser('p');
+
+            //create container for name, email, location
+            let employeeInfo = createUser('span');
+
+            //set image data
             img.src = employee.picture.medium;
+            //set employee name, email and city using placeholders 
             name.innerHTML = `${employee.name.first} ${employee.name.last}`;
             email.innerHTML = `${employee.email}`;
             location.innerHTML = `${employee.location.city}`;
+            //finally, append data to created elements
             li.appendChild(img);
+
+            
+
             li.appendChild(name);
             li.appendChild(email);
             li.appendChild(location);
             employees.appendChild(li);
+            //add class to set styling
             li.classList.add("employee-card");
         })
     })
