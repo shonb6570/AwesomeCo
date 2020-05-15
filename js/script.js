a87c8cea3298dca0b7b947b30789239c38b0042d
Shaun Bolak <47017803+shonb6570@users.noreply.github.com>
2020-05-15 17:22:42 -0400

    employee card img and info formatting fixed

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
         //"employees" is a <ul> element
 const employees = document.getElementById("employees");
-const url = 'https://randomuser.me/api/?results=12';
+const url = 'https://randomuser.me/api/?results=12&nat=us';
 
 // Fetch functions
 
@@ -20,30 +20,37 @@ function fetchInfo(url){
             //create html elements for API employee data  
             let li = createUser('li');
             let img = createUser('img');
-            let name = createUser('p');
+            let name = createUser('h3');
             let email = createUser('p');
             let location = createUser('p');
 
             //create container for name, email, location
-            let employeeInfo = createUser('span');
+            let employeeInfo = createUser('div');
 
             //set image data
             img.src = employee.picture.medium;
+
             //set employee name, email and city using placeholders 
             name.innerHTML = `${employee.name.first} ${employee.name.last}`;
             email.innerHTML = `${employee.email}`;
             location.innerHTML = `${employee.location.city}`;
-            //finally, append data to created elements
+
+            //append photo to new list item
             li.appendChild(img);
 
-            
+            //append name, email and location to separate <span> element 
+            //to style info in a column to the list item
+            employeeInfo.appendChild(name);
+            employeeInfo.appendChild(email);
+            employeeInfo.appendChild(location);
 
-            li.appendChild(name);
-            li.appendChild(email);
-            li.appendChild(location);
+            //append the li to the page
             employees.appendChild(li);
+            li.appendChild(employeeInfo);
+            
             //add class to set styling
             li.classList.add("employee-card");
+            employeeInfo.classList.add("employee-info");
         })
     })
     .catch(function (error) {
